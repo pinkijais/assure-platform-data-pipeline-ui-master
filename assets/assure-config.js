cfe29cb75b2f3a7c4bf6a256e82760c38db7ba6f
pinkijais <98391838+pinkijais@users.noreply.github.com>
2023-02-16 15:40:24 +0530

    Add files via upload

---
+++ assets/assure-config.js
@@ -0,0 +1,11 @@
+window.assureConfig = {
+cognitoPoolId: "us-east-1_g25Q8JE5M",
+cognitoAppClientId: "5l7i0njc7c9i5pkqo78f7dhnql",
+containerName: "platform-console",
+loginPath: "/login/index.html",
+platformconsoleApiBaseUrl: "/api/platform-console-api/",
+region: "us-east-1",
+uxconfigApiBaseUrl: "/api/data-lake/",
+apiHeaders: {"x-api-key": "d9cTvBrRoKDjEHmuQp1n0dVZ3hGcSyaexleXqp60"},
+dev: false
+};
